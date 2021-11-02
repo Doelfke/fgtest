d277c005a2bb78da9d27f6fb033e95e521bafacf
eXecute <execute@eXecutes-MacBook-Air.local>
2021-11-02 13:15:04 -0700

    basic formik setup

--- src/user/Register/Register.tsx
+++ src/user/Register/Register.tsx
@@ -1,6 +1,31 @@
 import React from 'react';
+import { Field, Formik, FormikProps } from 'formik';
+import { FormikValidatorBase, IsEmail, IsNotEmpty, IsString, MinLength } from 'formik-class-validator';
+
+class RegisterFormModel extends FormikValidatorBase {
+  @IsNotEmpty({message: 'Required'})
+  @IsString({message: 'Required'})
+  @MinLength(2,{message: 'Required'})
+  firstName: string = '';
+
+  @IsNotEmpty({message: 'Required'})
+  @IsString({message: 'Required'})
+  @IsEmail({},{message:'Invalid'})
+  email: string = '';
+  
+  @IsNotEmpty()
+  @IsNotEmpty({message: 'Required'})
+  @IsString({message: 'Required'})
+  @MinLength(5,{message: 'Too Short'})
+  password: string = '';
+}
 
 function Register() {
+
+  const handleSubmit = (values: RegisterFormModel) => {
+    console.log('submitting', values);
+  }
+
   return (
     <div>
       <h2>Let's</h2>
@@ -10,22 +35,32 @@ function Register() {
         You're only a few steps away!
       </p>
 
-      <div>
-        <label htmlFor="firstName">
-          First Name
-          <input name="firstName" />
-        </label>
-        <label htmlFor="email">
-          Email Address
-          <input name="email" />
-        </label>
-        <label htmlFor="password">
-          Password
-          <input name="password" />
-        </label>
-      </div>
+      <Formik initialValues={new RegisterFormModel()} onSubmit={handleSubmit} validate={RegisterFormModel.createValidator()} validateOnMount={true}>
+         {(formik: FormikProps<RegisterFormModel>) => (
+          <>
+            <label htmlFor="firstName">First Name</label>
+            <br />
+            <Field id="firstName" name="firstName" />
 
-      <button>Sign Up</button>
+            <br /> <br />
+
+            <label htmlFor="email">Email Address</label>
+            <br />
+            <Field id="email" name="email" />
+
+            <br /> <br />
+
+            <label htmlFor="password">Password</label>
+            <br />
+            <Field id="password" name="password" />
+
+            <br /> <br />
+
+            <button type="submit" onClick={()=>formik.handleSubmit()} disabled={!formik.isValid}>Submit</button>
+          </>
+        )}
+       </Formik>
+      
     </div>
   );
 }
